53a79647671848f4d0f66e4b522849c38ee22297
tihonov-ia <tia22@yandex.ru>
2024-07-14 21:59:45 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,16 @@
+import fake_match as fm
+import true_match as tm
+
+
+fake_divide = fm.divide
+true_divide = tm.divide
+
+result1 = fake_divide(69, 3)
+result2 = fake_divide(3, 0)
+result3 = true_divide(49, 7)
+result4 = true_divide(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
